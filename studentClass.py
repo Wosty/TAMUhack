f1884d695499596eb254b4b19e18113910e6257e
lwcreel <landon.creel@icloud.com>
2019-01-26 22:37:43 -0600

    created student and faceData classes

---
+++ studentClass.py
@@ -0,0 +1,12 @@
+# Stores data in class
+class student:
+    def __init__(self, name = "John Doe", id = 42 ):
+        self.stuName  = name
+        self.stuID    = id
+        #self.faceData = fd
+
+# Container for face data for student
+class faceData:
+    pass
+    # Data for face to go here when
+    # we know what to use
